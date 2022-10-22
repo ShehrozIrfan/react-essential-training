bfc2aa67c25aae5bd6fd0c0aed0579d8e140bf09
Shehroz Irfan <>
2022-10-22 19:49:15 +0500

    [Todo App] Adding the react-bootstrap tooltip

--- todo_app/src/components/Todos.js
+++ todo_app/src/components/Todos.js
@@ -1,18 +1,28 @@
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+
 const Todos = ({ items, handleDelete, handleComplete, handleEdit }) => {
   return (
     <div>
       {items.map((item) => (
-        <div
-          className="d-flex items-list"
-          key={item.id}
-          title="Double click to mark complete"
-        >
-          <div
-            className={item.completed ? "item-line-through" : ""}
-            onDoubleClick={() => handleComplete(item.id)}
+        <div className="d-flex items-list" key={item.id}>
+          <OverlayTrigger
+            delay={{ hide: 450, show: 300 }}
+            overlay={(props) => (
+              <Tooltip {...props}>
+                Double click to mark
+                {item.completed ? " todo" : " complete"}
+              </Tooltip>
+            )}
+            placement="bottom"
           >
-            {item.text}
-          </div>
+            <div
+              className={item.completed ? "item-line-through" : ""}
+              onDoubleClick={() => handleComplete(item.id)}
+            >
+              {item.text}
+            </div>
+          </OverlayTrigger>
           <div className="d-flex">
             <div>
               <button onClick={() => handleEdit(item)}>Edit</button>
