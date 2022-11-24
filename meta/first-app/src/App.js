aa02707797591d681519233204af86a530ce972f
Shehroz Irfan <>
2022-11-25 00:13:30 +0500

    [LAB_2] Solution added

--- meta/first-app/src/App.js
+++ meta/first-app/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import LAB_1 from "./components/LAB_1/LAB_1";
+import LAB_2 from "./components/LAB_2/LAB_2";
 
 function App() {
   return (
     <div>
       <LAB_1 />
+      <LAB_2 />
     </div>
   );
 }
