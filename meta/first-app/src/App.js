b70d04d9260c4b3b527c8b88f71fdf391984939c
Shehroz Irfan <>
2022-11-24 23:33:22 +0500

    [Week 1] Create a controlled form component

--- meta/first-app/src/App.js
+++ meta/first-app/src/App.js
@@ -1,9 +1,52 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [score, setScore] = useState(10);
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (score < 5 && comment.length < 10) {
+      alert("Please write in detail, why your experience was bad!");
+    }
+
+    setScore(10);
+    setComment("");
+  };
+  console.log(comment);
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Feedback Form</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Rating:</label>
+            <div>
+              <input
+                type="range"
+                value={score}
+                min="0"
+                max="10"
+                onChange={(e) => setScore(e.target.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <label>Comment:</label>
+            <div>
+              <textarea
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+              />
+            </div>
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
